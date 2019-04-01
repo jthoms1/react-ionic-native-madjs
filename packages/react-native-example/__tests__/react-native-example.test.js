08f6390f03b5a773f342b696b1ed28d012a87e15
Josh Thomas <josh@ionic.io>
2019-03-31 21:37:29 -0500

    Updates

--- packages/react-native-example/__tests__/react-native-example.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const reactNativeExample = require('..');
-
-describe('react-native-example', () => {
-    it('needs tests');
-});
--- packages/react-native-example/lib/react-native-example.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = reactNativeExample;
-
-function reactNativeExample() {
-    // TODO
-}
